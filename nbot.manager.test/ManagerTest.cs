90733ccfae82ca6d8f85ca9c295563b523aa0ef6
Paolo Gava <paolo.gava@exportfinance.gov.au>
2019-09-02 10:32:17 +1000

    Refactor test project

--- nbot.manager.test/ManagerTest.cs
+++ nbot.manager.test/ManagerTest.cs
@@ -1,33 +1,41 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
-using System.Threading.Tasks;
 using nbot.contracts;
 using Xunit;
 
 namespace nbot.manager.test
 {
-    public class UnitTest1
+    public class ManagerTest
     {
         [Fact]
-        public void Test1()
+        public void Can_Load_Bot_From_Assembly()
         {
-            var assemblyTask = AssemblyLoadContext.Default.LoadFromAssemblyPath(@"/home/paolo/Dev/robo/nbot/nbot.samples/bin/Debug/netstandard2.0\nbot.samples.dll");
+
+            // LoadFromAssemblyPath requires full path
+            var curDir = Directory.GetCurrentDirectory();
+            var path = Path.Combine(curDir, "../../../../nbot.samples/bin/Debug/netstandard2.0/nbot.samples.dll");
+
+            var assemblyTask = AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
 
             var task = CreateTask(assemblyTask);
 
             task.Run();
-            //Parallel.For(0, 10, (i) => { task.Run(null); });
+
+            //Parallel.For(0, 4, (i) => { task.Run(); });
             //ThreadPool.QueueUserWorkItem(task.Run);
 
         }
 
-        static INBot CreateTask(Assembly assembly)
+        static private INBot CreateTask(Assembly assembly)
         {
             int count = 0;
             IList<INBot> tasks = new List<INBot>();
+
+            // Find all objects of type INBot
             foreach (Type type in assembly.GetTypes())
             {
                 if (typeof(INBot).IsAssignableFrom(type))
@@ -41,7 +49,10 @@ namespace nbot.manager.test
                 }
             }
 
-
+            // Perform sanity check on assembly
+            // ...
+            
+            // Can have only one INBot
             if (count == 0)
             {
                 string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
@@ -49,16 +60,13 @@ namespace nbot.manager.test
                     $"Can't find any type which implements INBot in {assembly} from {assembly.Location}.\n" +
                     $"Available types: {availableTypes}");
             }
-
-
-            if (count > 1)
+            else if (count > 1)
             {
                 string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
                 throw new ApplicationException(
                     $"Too many types which implements INBot in {assembly} from {assembly.Location}.\n" +
                     $"Available types: {availableTypes}");
             }
-
 
             return tasks.First();
         }
