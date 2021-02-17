39fc6d4bdf79ee2436b15696031c2387e0303581
Paolo <paolo.gava@gmail.com>
2021-02-18 00:08:46 +1100

    Refactor implementation internal methods

--- nbot.actions/MoveActionsController.cs
+++ nbot.actions/MoveActionsController.cs
@@ -72,59 +72,56 @@ namespace nbot.actions
         
         public void CalculateNextPosition()
         {
+            // Check if ok to move
             if (forward == 0)
             {
                 return;
             }
 
-            currentDistance = CalculateDistance(currentDistance, currentLinearSpeed);
-            currentLinearSpeed = CalculateLinearSpeed();
-            currentAngularSpeed = CalculateAngularSpeed(Math.Abs(currentDistance), currentLinearSpeed);
-            currentPosition = CalculatePosition(currentDistance);
+            var nextLinearSpeed = CalculateLinearSpeed(currentLinearSpeed);
+            var nextDistance = CalculateDistance(currentDistance, nextLinearSpeed);
+            var nextAngularSpeed = CalculateAngularSpeed(nextDistance, nextLinearSpeed);
+            var nextPosition = CalculatePosition(currentPosition, nextAngularSpeed, nextDistance);
+
+            // Set the new current values
+            currentLinearSpeed = nextLinearSpeed;
+            currentDistance = nextDistance;
+            currentAngularSpeed = nextAngularSpeed;
+            currentPosition = nextPosition;
+
         }
-
-        private Vector CalculatePosition(double distance)
-        {
-            var direction = CalculateDirection(currentAngularSpeed);
-
-            return new Vector(positionProvider.CalculatePosition(currentPosition.Point(), direction, distance), direction);
-        }
-
-        private double CalculateLinearSpeed()
+        
+        private double CalculateLinearSpeed(double initialSpeed)
         {
             // Cannot go faster than the max speed
-            if (movementController.HasReachedMaxSpeed(currentLinearSpeed))
+            if (movementController.HasReachedMaxSpeed(initialSpeed))
             {
-                return currentLinearSpeed;
+                return initialSpeed;
             }
 
             // If the object is steering linear speed is fixed
-            if (steer != 0 && currentLinearSpeed != 0)
+            if (steer != 0 && initialSpeed != 0)
             {
-                return currentLinearSpeed;
+                return initialSpeed;
             }
 
-            return movementController.CalculateLinearSpeed(currentLinearSpeed);
+            return movementController.CalculateLinearSpeed(initialSpeed);
         }
 
-        private double CalculateAngularSpeed(double r, double linearSpeed)
-        {
-            return movementController.CalculateAngularSpeed(r, linearSpeed);
-        }
-
-        private double CalculateDistance(double currentDistance, double currentLinearSpeed)
+        private double CalculateDistance(double distance, double initialSpeed)
         {
             // If the object is turning keep distance fixed.
-            if (steer != 0 && currentDistance != 0)
+            if (steer != 0 && distance != 0)
             {
-                return currentDistance;
+                return distance;
             }
 
-            var distanceDelta = movementController.CalculateDistance(currentLinearSpeed);
+            var distanceDelta = movementController.CalculateDistance(initialSpeed);
 
             // Make sure we don't go further than value requested.
             var distanceNew = Math.Min(distanceDelta, Math.Abs(forward));
 
+            // Update how far the object should still move
             if (forward < 0)
             {
                 distanceNew *= -1;
@@ -135,12 +132,24 @@ namespace nbot.actions
             return distanceNew;
         }
 
-        private double CalculateDirection(double angularSpeed)
+        private double CalculateAngularSpeed(double distance, double initialSpeed)
+        {
+            return movementController.CalculateAngularSpeed(Math.Abs(distance), initialSpeed);
+        }
+
+        private Vector CalculatePosition(Vector position, double angularSpeed, double distance)
+        {
+            var direction = CalculateDirection(position, angularSpeed);
+
+            return new Vector(positionProvider.CalculatePosition(position.Point(), direction, distance), direction);
+        }
+        
+        private double CalculateDirection(Vector position, double angularSpeed)
         {
             // If not turning the direction is the same.
             if (steer == 0)
             {
-                return currentPosition.Direction;
+                return position.Direction;
             }
 
             var directionDelta = movementController.CalculateDirection(angularSpeed);
@@ -148,12 +157,13 @@ namespace nbot.actions
             // Make sure we don't go further than the value requested.
             directionDelta = Math.Min(directionDelta, Math.Abs(steer));
 
+            // Update how far the object should still turn
             if (steer < 0)
             {
                 directionDelta *= -1;
             }
 
-            var directionNew = currentPosition.Direction + directionDelta;
+            var directionNew = position.Direction + directionDelta;
 
             // Direction value is between 0-360. If the value is negative get the complement angle.
             if (directionNew < 0)
